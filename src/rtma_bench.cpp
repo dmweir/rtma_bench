26cbc8c70f3150a1fb6ebb85bac0588937822ce1
David Weir <dmw109@pitt.edu>
2020-07-16 19:35:35 -0400

    Add Test Timeout to catch dropped packets

--- src/rtma_bench.cpp
+++ src/rtma_bench.cpp
@@ -9,7 +9,11 @@
 
 
 #define MT_TEST_MSG 1234
-#define MT_SUBSCRIBER_READY 5678
+#define MT_PUBLISHER_READY 5677
+#define MT_PUBLISHER_DONE 5678
+#define MT_SUBSCRIBER_READY 5679
+#define MT_SUBSCRIBER_DONE 5680
+
 
 typedef struct {
 	char data[];
@@ -27,6 +31,7 @@ int subscriber_loop(int id, char* server, int num_msgs, int msg_size) {
 	
 	int msg_rcvd = 0;
 	std::chrono::time_point<std::chrono::high_resolution_clock> start;
+	std::chrono::time_point<std::chrono::high_resolution_clock> end;
 
 	mod.SendSignal(MT_SUBSCRIBER_READY);
 
@@ -39,6 +44,7 @@ int subscriber_loop(int id, char* server, int num_msgs, int msg_size) {
 			case MT_TEST_MSG:
 				if (msg_rcvd == 0)
 					start = std::chrono::high_resolution_clock::now();
+				end = std::chrono::high_resolution_clock::now();
 				msg_rcvd++;
 				break;
 			case MT_EXIT:
@@ -46,20 +52,31 @@ int subscriber_loop(int id, char* server, int num_msgs, int msg_size) {
 			}
 		}
 	}
+	mod.SendSignal(MT_SUBSCRIBER_DONE);
 
 quit:
-	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> dur = end - start;
 	double data_transfer = (double(msg_rcvd) - 1.0) * double(msg_size + sizeof(RTMA_MSG_HEADER)) / double(1024) / double(1024) / dur.count();
 
 	mod.DisconnectFromMMM();
 
-	printf("Subscriber[%d] -> %d messages | %d messages/sec | %0.1lf MB/sec | %0.6lf sec\n",
-		id,
-		msg_rcvd,
-		int((double(msg_rcvd) - 1.0)/dur.count()),
-		data_transfer,
-		dur.count());
+	if (msg_rcvd == num_msgs) {
+		printf("Subscriber[%d] -> %d messages | %d messages/sec | %0.1lf MB/sec | %0.6lf sec\n",
+			id,
+			msg_rcvd,
+			int((double(msg_rcvd) - 1.0) / dur.count()),
+			data_transfer,
+			dur.count());
+	}
+	else {
+		printf("Subscriber[%d] -> %d messages (%0d%%) | %d messages/sec | %0.1lf MB/sec | %0.6lf sec\n",
+			id,
+			msg_rcvd,
+			(int)((double(msg_rcvd) - 1.0)/ double(num_msgs) * 100.0),
+			int((double(msg_rcvd) - 1.0) / dur.count()),
+			data_transfer,
+			dur.count());
+	}
 
 	return 0;
 }
@@ -72,6 +89,8 @@ int publisher_loop(int id, char* server, int num_msgs, int msg_size, int num_sub
 	mod.Subscribe(MT_EXIT);
 	mod.Subscribe(MT_SUBSCRIBER_READY);
 	mod.SendModuleReady();
+
+	mod.SendSignal(MT_PUBLISHER_READY);
 
 	int subscribers_ready = 0;
 	while (subscribers_ready < num_subscribers) {
@@ -104,7 +123,9 @@ int publisher_loop(int id, char* server, int num_msgs, int msg_size, int num_sub
 
 		int status = mod.SendMessageRTMA(&M, (MODULE_ID)0 ,(HOST_ID)0);
 	}
-	
+
+	mod.SendSignal(MT_PUBLISHER_DONE);
+
 	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> dur = end - start;
 	double data_transfer = double(num_msgs) * double(msg_size + sizeof(RTMA_MSG_HEADER)) / double(1024) / double(1024) / dur.count();
@@ -174,6 +195,16 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	// Main Thread RTMA module
+	RTMA_Module mod;
+
+	mod.InitVariables(0, 0);
+	mod.ConnectToMMM(server);
+	mod.SendModuleReady();
+	mod.Subscribe(MT_PUBLISHER_READY);
+	mod.Subscribe(MT_PUBLISHER_DONE);
+	mod.Subscribe(MT_SUBSCRIBER_DONE);
+
 	std::vector<std::thread> publishers;
 	std::vector<std::thread> subscribers;
 
@@ -182,16 +213,60 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < num_publishers; i++)
 		publishers.push_back(std::thread(publisher_loop, i + 1, server, num_msgs / num_publishers, msg_size, num_subscribers));
 
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-
-	for (int i = 0; i < num_subscribers; i++)
-		subscribers.push_back(std::thread(subscriber_loop, i + 1, server, num_msgs, msg_size));
+	// Wait for publisher threads to be established
+	int publishers_ready = 0;
+	while (publishers_ready < num_publishers) {
+		CMessage M;
+		int status = mod.ReadMessage(&M, -1);
+		if (status) {
+			switch (M.msg_type) {
+			case MT_PUBLISHER_READY:
+				publishers_ready++;
+				break;
+			}
+		}
+	}
 
 	printf("Waiting for subscriber threads...\n");
+	for (int i = 0; i < num_subscribers; i++)
+		subscribers.push_back(std::thread(subscriber_loop, i + 1, server, num_msgs, msg_size));
 
 	printf("Starting Test...\n");
 
 	printf("Total RTMA Packet Size: %d bytes\n", sizeof(RTMA_MSG_HEADER) + msg_size);
+
+
+	//Wait for subscribers to finish
+	double abort_timeout = max(num_msgs / 10000.0, 10.0);
+	auto abort_start = std::chrono::high_resolution_clock::now();
+
+	int subscribers_done = 0;
+	int publishers_done = 0;
+
+	while ( (subscribers_done < num_subscribers) && (publishers_done < num_publishers) ) {
+		CMessage M;
+		int status = mod.ReadMessage(&M, 0.100);
+		if (status) {
+			switch (M.msg_type) {
+			case MT_SUBSCRIBER_DONE:
+				subscribers_done++;
+				break;
+			case MT_PUBLISHER_DONE:
+				publishers_done++;
+				break;
+			}
+		}
+
+		auto now = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> dur = now - abort_start;
+
+		if (dur.count() > abort_timeout) {
+			printf("Test Timeout! Sending Exit Signal...\n");
+			mod.SendSignal(MT_EXIT);
+			break;
+		}
+	}
+
 	for (auto& publisher : publishers)
 		publisher.join();
 
